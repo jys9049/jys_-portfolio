52962ee742361fc5dfbe6de92aa32c7cb9432709
jys9049 <github.com.com/jys9049>
2024-06-13 19:01:41 +0900

    setting: 프로젝트 초기 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
